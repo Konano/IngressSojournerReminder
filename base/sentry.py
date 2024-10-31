87d4538c46edc19eaf623882400bca4689f670e1
Konano <nanoapezlk@gmail.com>
2024-10-31 11:00:04 +0800

    fix(sentry): minor in Sentry init

--- base/sentry.py
+++ base/sentry.py
@@ -15,14 +15,22 @@ def sentry_init():
         return
     SENTRY_INIT = True
 
-    dsn = config['SENTRY'].get('dsn')
-    if dsn is None:
+    if 'SENTRY' not in config or config['SENTRY'].get('dsn') is None:
         return
 
     sentry_sdk.init(
-        dsn=dsn,
+        dsn=config['SENTRY']['dsn'],
         release=datetime.now().strftime('%Y-%m-%d'),
         attach_stacktrace=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        _experiments={
+            # Set continuous_profiling_auto_start to True
+            # to automatically start the profiler on when
+            # possible.
+            "continuous_profiling_auto_start": True,
+        },
     )
 
     shlr = SentryHandler()
